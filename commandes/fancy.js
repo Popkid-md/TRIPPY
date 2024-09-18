89ff7abc72b6b80ecfad8d2cf73a0f549c74becb
Popkid-md <weanslatevanessa@gmail.com>
2024-09-18 09:26:57 +0300

    Popkid😈

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 2 𝐓𝐫𝐞𝐱 𝐌𝐝\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('𝐓𝐫𝐞𝐱 𝐌𝐝', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 2 popkid 𝐌𝐝\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('popkid 𝐌𝐝', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
