2257b09a35a4c0b9bdd01f5e4875dd2d3c280f8c
Popkid-md <weanslatevanessa@gmail.com>
2024-09-18 10:01:17 +0300

    Popkid😈

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 
 
 
-zokou({nomCom:"bera",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"popkid",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
