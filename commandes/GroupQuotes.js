ca935df31be1f0d8006caf1e098e270dc4012694
Popkid-md <weanslatevanessa@gmail.com>
2024-09-18 09:57:08 +0300

    Popkid😈

--- commandes/GroupQuotes.js
+++ commandes/GroupQuotes.js
@@ -13,13 +13,13 @@ zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions)
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*Bugatti Quotes*
+          repondre(`*popkid Quotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *Bugatti*`);
+Powered by *popkid*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -31,13 +31,13 @@ Powered by *Bugatti*`);
       fetch('https://animechan.xyz/api/random/character?name=' + query)
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`Hacking-MD
+          repondre(`popkid-MD
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *Bugatti*`);
+Powered by *popkid*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
